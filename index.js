95c7cb389eb04a97e53e91abc5be1c5066961027
Mouni Nischal <d.chiranjeevi007@gmail.com>
2020-11-01 10:32:19 +0530

    abe to send mail now

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 // Importing File System and Readline to take input and store credentials in file
-const fs = require('fs');
-const readline = require('readline');
-var { google } = require('googleapis');
+const fs = require("fs");
+const readline = require("readline");
+var { google } = require("googleapis");
 
 // Creating variable to control the set of resources
 // and operations that an access token permits
 const SCOPES = [
-	'https://www.googleapis.com/auth/gmail.readonly',
-	'https://www.googleapis.com/auth/userinfo.email',
-	'https://www.googleapis.com/auth/userinfo.profile',
+	"https://www.googleapis.com/auth/gmail.readonly",
+	"https://www.googleapis.com/auth/userinfo.email",
+	"https://www.googleapis.com/auth/userinfo.profile",
+	"https://mail.google.com/",
+	"https://www.googleapis.com/auth/gmail.modify",
+	"https://www.googleapis.com/auth/gmail.compose",
+	"https://www.googleapis.com/auth/gmail.send",
 ];
 
 // File name to store the token
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = "token.json";
 
 /*!
     First off, to use OAuth2 authentication, we will need access to
@@ -23,12 +27,12 @@ const TOKEN_PATH = 'token.json';
 */
 
 // Getting the client secrets from the local file "credentials.json"
-fs.readFile('credentials.json', (err, content) => {
+fs.readFile("credentials.json", (err, content) => {
 	// Handling errors (if any)
-	if (err) return console.log('Error parsing credentials file:', err);
+	if (err) return console.log("Error parsing credentials file:", err);
 
-    // Calling the authorize function to authorize the client's credentials,
-    // then use the callback function to send an email
+	// Calling the authorize function to authorize the client's credentials,
+	// then use the callback function to send an email
 	authorize(JSON.parse(content), sendMail);
 });
 
@@ -45,7 +49,7 @@ function authorize(credentials, callback) {
 	);
 
 	// Configuring googleapis tpo use authentication credentials
-	google.options({ auth: oauth2Client });
+	google.options({ auth: oAuth2Client });
 
 	// Check if we have previously stored a token.
 	fs.readFile(TOKEN_PATH, (err, token) => {
@@ -67,11 +71,11 @@ function getNewToken(oAuth2Client, callback) {
 	// Generating the authorization URL with offline access and respective scope
 	const authUrl = oAuth2Client.generateAuthUrl({
 		// Here we have offline access which uses a refresh token
-		access_type: 'offline',
+		access_type: "offline",
 		scope: SCOPES,
 	});
 	console.log(
-		'You must authorize this app to access your credentials by visiting this url: ',
+		"You must authorize this app to access your credentials by visiting this url: ",
 		authUrl
 	);
 
@@ -81,13 +85,13 @@ function getNewToken(oAuth2Client, callback) {
 		output: process.stdout,
 	});
 	input.question(
-		'Enter the access token from the redirected page: ',
+		"Enter the access token from the redirected page: ",
 		(code) => {
 			input.close();
 			oAuth2Client.getToken(code, (err, token) => {
 				// Handling error (if any) while getting token from API
 				if (err) {
-					return console.error('Error receiving access token', err);
+					return console.error("Error receiving access token", err);
 				}
 				// Creating the credentials
 				oAuth2Client.setCredentials(token);
@@ -97,7 +101,7 @@ function getNewToken(oAuth2Client, callback) {
 						return console.error(err);
 					}
 					console.log(
-						'User access token stored offline to ',
+						"User access token stored offline to ",
 						TOKEN_PATH
 					);
 				});
@@ -107,23 +111,71 @@ function getNewToken(oAuth2Client, callback) {
 	);
 }
 
-async function sendMail(auth) {
-	const gmail = google.gmail({ version: 'v1', auth });
-	gmail.users.labels.list(
+function makeBody(to, from, subject, message) {
+	var str = [
+		'Content-Type: text/plain; charset="UTF-8"\n',
+		"MIME-Version: 1.0\n",
+		"Content-Transfer-Encoding: 7bit\n",
+		"to: ",
+		to,
+		"\n",
+		"from: ",
+		from,
+		"\n",
+		"subject: ",
+		subject,
+		"\n\n",
+		message,
+	].join("");
+
+	var encodedMail = new Buffer.from(str)
+		.toString("base64")
+		.replace(/\+/g, "-")
+		.replace(/\//g, "_");
+	return encodedMail;
+}
+
+function sendMail(auth) {
+	const gmail = google.gmail({ version: "v1", auth });
+	var raw = makeBody(
+		"d.chiranjeevi007@gmail.com",
+		"d.chiranjeevi007@gmail.com",
+		"This is your subject",
+		"I got this working finally!!!"
+	);
+
+	let mail = gmail.users.messages.send(
 		{
-			userId: 'me',
+			auth: auth,
+			userId: "me",
+			resource: {
+				raw: raw,
+			},
 		},
-		(err, res) => {
-			if (err) return console.log('The API returned an error: ' + err);
-			const labels = res.data.labels;
-			if (labels.length) {
-				console.log('Labels:');
-				labels.forEach((label) => {
-					console.log(`- ${label.name}`);
-				});
-			} else {
-				console.log('No labels found.');
-			}
+		function (err, response) {
+			return err || response;
 		}
 	);
+
+	return mail;
 }
+
+// Starting the server
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 5500;
+
+// // Using express for routing
+// var express = require("express");
+// var app = express()
+
+const server = http.createServer((req, resp) => {
+	resp.statusCode = 200;
+	resp.setHeader("Content-Type", "text/plain");
+	resp.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
