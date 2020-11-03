d2100cd2a83bd88bb4ccc4878ba7edbeb9ce3aca
Mouni Nischal <d.chiranjeevi007@gmail.com>
2020-11-03 21:47:52 +0530

    Finished sending email smoothly

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const fs = require("fs");
 const readline = require("readline");
 var { google } = require("googleapis");
+const http = require("http");
+const destroyer = require("server-destroy");
+const url = require("url");
+const opn = require("open");
 
 // Creating variable to control the set of resources
 // and operations that an access token permits
@@ -27,90 +31,80 @@ const TOKEN_PATH = "token.json";
 */
 
 // Getting the client secrets from the local file "credentials.json"
-fs.readFile("credentials.json", (err, content) => {
-	// Handling errors (if any)
-	if (err) return console.log("Error parsing credentials file:", err);
+var content = fs.readFileSync("credentials.json");
+const credentials = JSON.parse(content);
+// Storing the credentials in variables
+const { client_secret, client_id, redirect_uris } = credentials.web;
+// Creating a new OAuth2 client using the user's login keys
+const oAuth2Client = new google.auth.OAuth2(
+	client_id,
+	client_secret,
+	redirect_uris[0]
+);
+// Configuring googleapis tpo use aOAuth2 client
+google.options({ auth: oAuth2Client });
 
-	// Calling the authorize function to authorize the client's credentials,
-	// then use the callback function to send an email
-	authorize(JSON.parse(content), sendMail);
-});
 
 // Creating the OAuth2 client and then executing the callback function
-function authorize(credentials, callback) {
-	// Storing the credentials in variables
-	const { client_secret, client_id, redirect_uris } = credentials.web;
+async function authenticate(scopes) {
+	return new Promise((resolve, reject) => {
+		// grab the url that will be used for authorization
+		const authorizeUrl = oAuth2Client.generateAuthUrl({
+			access_type: "offline",
+			scope: scopes.join(" "),
+		});
+		const server = http
+			.createServer(async (req, res) => {
+				try {
+					if (req.url.indexOf("/oauth2callback") > -1) {
+						const qs = new url.URL(req.url, "http://localhost:3000")
+							.searchParams;
+						res.end(
+							"Authentication successful! Please return to the console."
+						);
+						server.destroy();
 
-	// Creating a new OAuth2 client using the user's login keys
-	const oAuth2Client = new google.auth.OAuth2(
-		client_id,
-		client_secret,
-		redirect_uris[0]
-	);
+						const { tokens } = await oAuth2Client.getToken(
+							qs.get("code")
+						);
 
-	// Configuring googleapis tpo use authentication credentials
-	google.options({ auth: oAuth2Client });
+						// Creating the credentials
+						oAuth2Client.credentials = tokens;
 
-	// Check if we have previously stored a token.
-	fs.readFile(TOKEN_PATH, (err, token) => {
-		// If we have previously set a token we will
-		// need to overwrite it and create a new one.
-		if (err) return getNewToken(oAuth2Client, callback);
-
-		// Else creating the credentials
-		oAuth2Client.setCredentials(JSON.parse(token));
-		callback(oAuth2Client);
-	});
-}
-
-/*
-    Getting the user token after getting user authorization and storing
-    the toke in the file under TOKEN_PATH variable name. Then executing the callback function with the now authorized OAuth2 Client
-*/
-function getNewToken(oAuth2Client, callback) {
-	// Generating the authorization URL with offline access and respective scope
-	const authUrl = oAuth2Client.generateAuthUrl({
-		// Here we have offline access which uses a refresh token
-		access_type: "offline",
-		scope: SCOPES,
-	});
-	console.log(
-		"You must authorize this app to access your credentials by visiting this url: ",
-		authUrl
-	);
-
-	// Getting user input of the accessToken
-	const input = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-	input.question(
-		"Enter the access token from the redirected page: ",
-		(code) => {
-			input.close();
-			oAuth2Client.getToken(code, (err, token) => {
-				// Handling error (if any) while getting token from API
-				if (err) {
-					return console.error("Error receiving access token", err);
-				}
-				// Creating the credentials
-				oAuth2Client.setCredentials(token);
-				// Storing the obtained credentials into a file.
-				fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-					if (err) {
-						return console.error(err);
+						// Checking if user's toke is already stored
+						fs.readFile(TOKEN_PATH, (err, token) => {
+							if (err) {
+								// Storing the obtained credentials into a file.
+								fs.writeFile(
+									TOKEN_PATH,
+									JSON.stringify(tokens),
+									(err) => {
+										if (err) {
+											return console.error(err);
+										}
+										console.log(
+											"User access token stored offline to ",
+											TOKEN_PATH
+										);
+									}
+								);
+							}
+						});
+						resolve(oAuth2Client);
 					}
-					console.log(
-						"User access token stored offline to ",
-						TOKEN_PATH
-					);
-				});
-				callback(oAuth2Client);
+				} catch (e) {
+					reject(e);
+				}
+			})
+			.listen(3000, () => {
+				// open the browser to the authorize url to start the workflow
+				opn(authorizeUrl, { wait: false }).then((cp) => cp.unref());
 			});
-		}
-	);
+		destroyer(server);
+	});
 }
 
+// Function to format the mail headers and body
 function makeBody(to, from, subject, message) {
 	var str = [
 		'Content-Type: text/plain; charset="UTF-8"\n',
@@ -138,10 +132,10 @@ function makeBody(to, from, subject, message) {
 function sendMail(auth) {
 	const gmail = google.gmail({ version: "v1", auth });
 	var raw = makeBody(
-		"d.chiranjeevi007@gmail.com",
-		"d.chiranjeevi007@gmail.com",
-		"This is your subject",
-		"I got this working finally!!!"
+		"d.chiranjeevi007@gmail.com", // To address
+		"d.chiranjeevi007@gmail.com", // From address (only the user's email id works)
+		"TEsting", // Subject of the mail
+		"I have finished my work successfully" // Content of the mail
 	);
 
 	let mail = gmail.users.messages.send(
@@ -160,22 +154,6 @@ function sendMail(auth) {
 	return mail;
 }
 
-// Starting the server
-const http = require("http");
-
-const hostname = "127.0.0.1";
-const port = 5500;
-
-// // Using express for routing
-// var express = require("express");
-// var app = express()
-
-const server = http.createServer((req, resp) => {
-	resp.statusCode = 200;
-	resp.setHeader("Content-Type", "text/plain");
-	resp.end("Hello World");
-});
-
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+authenticate(SCOPES)
+	.then((client) => sendMail())
+	.catch(console.error);
